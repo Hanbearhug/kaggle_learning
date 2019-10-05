dbdb3c9954c20119e28c57473ebd5479d79cee11
Hanbearhug <38373160+Hanbearhug@users.noreply.github.com>
2019-10-05 14:46:29 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,43 @@
+# Model
+def run_cv_model(train, test, target, model_fn, params={}, eval_fn=None, label='model'):
+    kf = KFold(n_splits=5)
+    fold_splits = kf.split(train, target)
+    cv_scores = []
+    pred_full_test = 0
+    pred_train = np.zeros((train.shape[0]))
+    i = 1
+    for dev_index, val_index in fold_splits:
+        print('Started ' + label + ' fold ' + str(i) + '/5')
+        dev_X, val_X = train[dev_index], train[val_index]
+        dev_y, val_y = target[dev_index], target[val_index]
+        params2 = params.copy()
+        pred_val_y, pred_test_y = model_fn(dev_X, dev_y, val_X, val_y, test, params2)
+        pred_full_test = pred_full_test + pred_test_y
+        pred_train[val_index] = pred_val_y
+        if eval_fn is not None:
+            cv_score = eval_fn(val_y, pred_val_y)
+            cv_scores.append(cv_score)
+            print(label + ' cv score {}: {}'.format(i, cv_score))
+        i += 1
+    total_cv = eval_fn(target, pred_train)
+    print('{} cv scores : {}'.format(label, cv_scores))
+    print('{} cv mean score : {}'.format(label, np.mean(cv_scores)))
+    print('{} cv std score : {}'.format(label, np.std(cv_scores)))
+    print('{} cv score : {}'.format(label, total_cv)
+    pred_full_test = pred_full_test / 5.0
+    results = {'label': label,
+              'train': pred_train, 'test': pred_full_test,
+              'cv': cv_scores,
+              'total_cv': total_cv}
+    return results
+
+
+def runLR(train_X, train_y, test_X, test_y, test_X2, params):
+    print('Train LR')
+    model = LogisticRegression(**params)
+    model.fit(train_X, train_y)
+    print('Predict 1/2')
+    pred_test_y = model.predict_proba(test_X)[:, 1]
+    print('Predict 2/2')
+    pred_test_y2 = model.predict_proba(test_X2)[:, 1]
+    return pred_test_y, pred_test_y2
